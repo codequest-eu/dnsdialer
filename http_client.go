279d9597c844f5f5c268357d0eaf5519443015c9
Marcin Wyszynski <marcin.pixie@gmail.com>
2016-09-06 15:55:20 +0200

    Make the library compatible with Go 1.6

--- http_client.go
+++ http_client.go
@@ -12,8 +12,6 @@ func HTTPClient(dnsResolvers []net.Addr) *http.Client {
 		Transport: &http.Transport{
 			Proxy:                 http.ProxyFromEnvironment,
 			Dial:                  dialer.Dial,
-			MaxIdleConns:          100,
-			IdleConnTimeout:       90 * time.Second,
 			TLSHandshakeTimeout:   10 * time.Second,
 			ExpectContinueTimeout: 1 * time.Second,
 		},
